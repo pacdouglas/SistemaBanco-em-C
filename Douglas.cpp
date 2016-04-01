7731e9b55f7a1a5492b704cdbca139e78f7e0ce8
Douglas Martins <douglas.pac@gmail.com>
2016-04-01 00:59:53 -0300

    Trabalho Entregue

---
+++ Douglas.cpp
@@ -0,0 +1,20 @@
+/***********************************************************
+* Nome Aluno: Douglas Martins		RA:41045922854 *
+* Data: 28/05/2013									*
+* Resumo do problema: Trabalho02 - Controle bancário *
+************************************************************/
+#include <iostream.h>
+#include <conio.h>
+#include <locale.h>
+#include <iomanip>
+void tela(){
+       
+main()
+{       
+//Personalização de Cor
+system("color 17");
+//Configurando Idioma
+setlocale(LC_ALL, "Portuguese");       
+       
+}
+
